edb824a377223075b70ed3965421782a669339d2
Alex Oancea <alx.oancea@gmail.com>
2018-04-09 01:46:58 +0300

    install ganache-cli. deploy contract. currently not working as the contract has been deployed with 30sec auction time which has exceeded. in order to work we need to be able to deploy a contract on page load.

--- src/app/services/ethereum-provider.service.ts
+++ src/app/services/ethereum-provider.service.ts
@@ -3,12 +3,16 @@ import { Injectable } from '@angular/core';
 declare var Eth: any;
 declare var TestRPC: any;
 
+declare var Web3: any;
+
 @Injectable()
 export class EthereumProviderService {
 
     EthInstance: any;
 
+    Web3: any;
+
     constructor() {
-        this.EthInstance = new Eth(TestRPC.provider());
+        this.EthInstance = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
      }
 }
