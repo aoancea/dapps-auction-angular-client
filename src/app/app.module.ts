86c74ef93492fc85a01fe6130469b6258a6872ba
Alex Oancea <alx.oancea@gmail.com>
2018-04-08 10:26:07 +0300

    adding auction route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AuctionComponent } from './auction/auction.component';
 
+const routes = [
+    { path: 'auction', component: AuctionComponent },
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AuctionComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
