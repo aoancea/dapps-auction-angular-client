d4eeee9827297af91cb9eda6f9da9d3079eabac8
Alex Oancea <alx.oancea@gmail.com>
2018-04-09 23:37:47 +0300

    bind auction information through angular 2

--- src/app/auction/auction.component.ts
+++ src/app/auction/auction.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-auction',
-  templateUrl: './auction.component.html',
-  styleUrls: ['./auction.component.less']
+    selector: 'app-auction',
+    templateUrl: './auction.component.html',
+    styleUrls: ['./auction.component.less']
 })
 export class AuctionComponent implements OnInit {
 
-  constructor() { }
+    private beneficiary: string = 'Loading ...';
+    private raised: string = 'Loading ...';
+    private time_left: string = 'Loading ...';
 
-  ngOnInit() {
-  }
+    private highest_bidder: string;
+    private highest_bid: string;
+
+    private account_balance: string = 'Loading ...';
+    private account_address: string = 'Loading ...';
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
 
 }
