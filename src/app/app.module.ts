3c3536ccf866d8525d68a90bde97d900edb58497
Alex Oancea <alx.oancea@gmail.com>
2018-04-10 21:36:10 +0300

    clean-up unused pages and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { EthereumProviderService } from './services/ethereum-provider.service';
 import { Web3ProviderService } from './services/web3-provider.service';
 
 import { AppComponent } from './app.component';
 
 import { AuctionComponent } from './auction/auction.component';
-import { JsAuctionComponent } from './js-auction/js-auction.component';
 
 const routes = [
-    { path: 'auction', component: AuctionComponent },
-    { path: 'js-auction', component: JsAuctionComponent },
+    { path: 'auction', component: AuctionComponent }
 ]
 
 @NgModule({
     declarations: [
         AppComponent,
-        AuctionComponent,
-        JsAuctionComponent
+        AuctionComponent
     ],
     imports: [
         BrowserModule,
         RouterModule.forRoot(routes),
         FormsModule
     ],
-    providers: [EthereumProviderService, Web3ProviderService],
+    providers: [Web3ProviderService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
